52aab852869b0fa996f131716bcd92af4e3668d2
Prakruti panchal <prakrutipanchal2005@gmail.com>
2025-07-16 14:30:51 +0530

    Added test for adding sweets

---
+++ tests/Sweet_test.js
@@ -0,0 +1,23 @@
+import Sweet from "../src/models/Sweet.js";
+
+describe('Sweet', () => {
+  test('should create a sweet with all required properties', () => {
+    const sweet = new Sweet(1, 'Chocolate Bar', 'chocolate', 1.99, 50);
+    
+    expect(sweet.id).toBe(1);
+    expect(sweet.name).toBe('Chocolate Bar');
+    expect(sweet.category).toBe('chocolate');
+    expect(sweet.price).toBe(1.99);
+    expect(sweet.quantity).toBe(50);
+  });
+
+  test('should throw error when quantity is negative', () => {
+    expect(() => new Sweet(1, 'Chocolate Bar', 'chocolate', 1.99, -1))
+      .toThrow('Quantity cannot be negative');
+  });
+
+  test('should throw error when price is negative', () => {
+    expect(() => new Sweet(1, 'Chocolate Bar', 'chocolate', -1.99, 10))
+      .toThrow('Price cannot be negative');
+  });
+});
